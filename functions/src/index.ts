d3be6cc819462af2d226c50462a60a12666900d9
Tommy Su <81478928+helmet323@users.noreply.github.com>
2024-12-28 18:04:02 -0500

    removed functions

--- functions/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { onRequest } from "firebase-functions/v2/https";
-
-// Simple HTTP function to return "Hello, Firebase!".
-export const helloWorld = onRequest((req, res) => {
-  res.send("Hello, Firebase!");
-});
