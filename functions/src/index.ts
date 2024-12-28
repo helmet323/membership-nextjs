db658a714331da522e3672f7526956a73e493aa5
Tommy Su <81478928+helmet323@users.noreply.github.com>
2024-12-28 17:57:04 -0500

    added firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,6 @@
 import { onRequest } from "firebase-functions/v2/https";
 
-type Indexable = { [key: string]: any };
-
-export const helloworld = onRequest((req, res) => {
-  const name = req.params[0];
-  const items: Indexable = { lamp: "This is a lamp", chair: "Good chair" };
-  const message = items[name];
-  res.send(`<h1>${message}</h1>`);
+// Simple HTTP function to return "Hello, Firebase!".
+export const helloWorld = onRequest((req, res) => {
+  res.send("Hello, Firebase!");
 });
